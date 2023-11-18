cf02394198a8ff35a383d0dd6b2ae05011b3c3b4
Edgar <espinosamartinedgar@gmail.com>
2023-11-18 23:10:24 +0100

    Se corrigen errores de importación y queda preparado el script a falta de adaptarlo al host y demás elementos

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import threading
-import servidorHTTS
-import servidorSSH
-import sockets
+from servidorHTTS.httpserver import run_server
+from servidorSSH.sshserver import create_ssh_tunnel
+from sockets.sockets import main as socket_main
+
 
 def main():
     # Iniciar el servidor HTTP en un hilo separado
-    threading.Thread(target=servidorHTTS.run_server, daemon=True).start()
+    threading.Thread(target=run_server, daemon=True).start()
 
     # Establecer la conexión SSH y configurar el reenvío de puertos
-    servidorSSH.create_ssh_tunnel()
+    # Suponiendo que esto no necesita ejecutarse en un hilo separado
+    create_ssh_tunnel()
 
     # Iniciar el servidor de sockets
-    sockets.main()
+    # Suponiendo que esto no necesita ejecutarse en un hilo separado
+    socket_main()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
